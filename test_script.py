7c9a20ef251502e21a0d0feb25d735bfe41dceb7
WestMare <40365864+WestMare@users.noreply.github.com>
2023-07-30 12:30:05 +0700

    Script fix

--- test_script.py
+++ test_script.py
@@ -1,23 +1,23 @@
 import Gitlab
 
-access_token = 'Token'
-
-print "Введите ваш токен")
+print ("Введите ваш токен")
 gitlab_token = input()
 print("Введите URL")
 gitlab_url = input()
 
-
-Gitlab(
-        "https://{gitlab_url}/api/v4".format(gitlab_url=gitlab_url),
-        private_token=github_token)
+api_url = "https://{gitlab_url}/api/v4".format(gitlab_url=gitlab_url)
+Gitlab(api_url, private_token=gitlab_token)
         
-
-gl = gitlab.gitlab(url = gitlab_url, private_token = access_token)
+gl = gitlab.Gitlab(url = gitlab_url, private_token = access_token)
 users = gl.users.list(page = 1,per_page=1000)
 
 for user in users:
-    if (not(user.is_admin)
+    if (not(user.is_admin)):
         try:
-            block_user(user) continue 
+            user.block()
+            print('Пользователь ' + user.username + ' заблокирован')
+            continue
+        except:
+            print('Не удалось заблокировать пользователя '+ user.username)
+                
 print('Все пользователи без права администрирования заблокированы')
